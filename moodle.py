95dd856c30ea6dd82761b2d4ecc16cd38ea47966
gnramos <ramos@gnramos.com>
2021-05-13 12:06:46 -0300

    Inclusão de classe para lidar com participação discente. Refatoração de documentação para Português.

--- moodle.py
+++ moodle.py
@@ -1,90 +1,119 @@
 import csv
+import json
 import locale
 import os
+import re
 
 
 class MoodleBase(dict):
-    """Base class for handling student information from Moodle reports.
+    """Classe base para manipulação de dados de relatórios do Moodle.
 
-    Extends dict, so keys are the student IDs and each item is a dict with (at
-    least) a 'Name' key with the student's name.
+    Funciona como um dicionário, de modo que as chaves são as matrículas e cada
+    item é um dicionário com pelo menos a chave 'Name' contendo o nome do
+    discente.
+
     """
 
-    def __init__(self):
-        self._sorted_keys = sorted(self.keys(), key=lambda x: locale.strxfrm(
-            self[x]['Name']))
-
     def __str__(self):
-        return '\n'.join(f'{self[k]["Name"]}, {k}' for k in self._sorted_keys)
+        return '\n'.join(f'{self[k]["Name"]}, {k}' for k in self.sorted_keys())
+
+    def sorted_keys(self):
+        """Itera pelas chaves do dicionário em ordem alfabética."""
+        for k in sorted(self.keys(),
+                        key=lambda x: locale.strxfrm(self[x]['Name'])):
+            yield k
 
 
 class Grades(MoodleBase):
-    """Handle participants activity completion information.
+    """Processa o relatório de notas discentes no curso.
 
-    Processes a Moodle report data to extract participants activity completion
-    information. Extends dict, so keys are the student IDs and each item has
-    the following structure:
-        {'Name': (str - student full name),
-         'Notas': (dict)
-            {activity (str): grade (float)}}
+    Acrescenta as notas grades_idx das atividades avaliadas e afinal do discente.
 
-    To get the data file:
-        1. Access the Grades report via:
-            Course > This Course > Grades > Export (tab).'
-        2. Select the "Text File" format (tab).
-        3. Select the desired group.
-        4. Select the desired items.
-        5. Select the export format option: "comma".
+    Para obter o arquivo:
+        1. Acesse o relatório via:
+            Este Curso > Notas > Exportar (aba)
+        2. Selecione o formato "Arquivo texto" (aba).
+        3. Selecione o grupo desejado, se for o caso.
+        4. Selecione os itens que se deseja exportar.
+        5. Selecione o formato de exportação "Real" com separador "Vírgula".
     """
 
     def __init__(self, file):
-        """Constructor.
+        """Construtor.
 
-        Loads the data from the file into instance.
+        Carrega os dados do arquivo.
 
-        Arguments:
-        file -- the CSV file to read from.
+        Argumentos:
+        file -- o arquivo CSV a ser lido.
         """
 
         with open(file) as csvfile:
             csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
             header = next(csvreader)  # skip header
-            totais = [i for i, col in enumerate(header) if col.endswith(' total (Real)')]
-            totais.append(header.index('Nota Final (Real)'))
+            grades_idx = [i for i, col in enumerate(header)
+                          if col.endswith(' total (Real)')]
+            grades_idx.append(header.index('Nota Final (Real)'))
 
             for row in csvreader:
-                first_name, last_names, student_id = row[:3]
-                self[student_id] = {'Name': f'{first_name} {last_names}',
-                                    'Notas': {header[i]: float(row[i]) for i in totais}}
+                first_name, last_names, s_id = row[:3]
+                self[s_id] = {'Name': f'{first_name} {last_names}'}
+                for i in grades_idx:
+                    self[s_id][header[i]] = float(row[i])
 
-        super().__init__()
 
+class Participants(MoodleBase):
+    """Processa o relatório de participação discente no curso.
 
-class Progress(MoodleBase):
-    """Handle participants activity completion information.
+    Acrescenta a chave 'Group' que indica os grupos (turma) do discente.
 
-    Processes a Moodle report data to extract participants activity completion
-    information. Extends dict, so keys are the student IDs and each item has
-    the following structure:
-        {'Name': (str - student full name),
-         'Frequência': (int - student participation (%)),
-         'Faltas':  (int - student absence (%))}
+    Para obter o arquivo:
+        1. Acesse o relatório via:
+            Este Curso > Pessoas
+        2. Aperte o botão "Selecionar todos os X usuários".
+        3. Salve a página como HTML (completo).
 
-    To get the data file:
-        1. Access the Activity Completion report via:
-            Course administration > Reports > Activity completion.'
-        2. Select the desired group.
-        3. Download spreadsheet format (UTF-8 .csv).
+    Apenas o arquivo HTML é necessário.
     """
 
     def __init__(self, file):
-        """Constructor.
+        """Construtor.
 
-        Loads the data from the file into instance.
+        Carrega os dados do arquivo.
 
-        Arguments:
-        file -- the CSV file to read from.
+        Argumentos:
+        file -- o arquivo HTML a ser lido.
+        """
+
+        with open(file) as htmlfile:
+            html = htmlfile.read()
+
+        pattern = (r'<label for=.*?user\d+.*?<img.*?>(.*?)</a>.*?'
+                   r'(\d+)@aluno.unb.br[.\s\S]*?title="Editar grupos.*?>[\s\S]'
+                   r' +(\w.*)[\s\S]')
+        for name, s_id, group in re.findall(pattern, html):
+            self[s_id] = {'Name': name, 'Group': group}
+
+
+class Progress(MoodleBase):
+    """Processa o relatório de progresso de discentes no curso.
+
+    Acrescenta "Frequência" e "Faltas" do discente, em termos percentuais.
+
+    Para obter o arquivo:
+        1. Acesse o relatório via:
+            Administração do curso > Relatórios (aba) > Conclusão de atividades
+        2. Selecione o grupo desejado, se for o caso.
+        3. Faça o download em formato de planilha (UTF-8. csv).
+    """
+
+    def __init__(self, file):
+        """Construtor.
+
+        Carrega os dados do arquivo.
+
+        Argumentos:
+        file -- o arquivo CSV a ser lido.
         """
 
         with open(file) as csvfile:
@@ -94,123 +123,91 @@ class Progress(MoodleBase):
             activities = (len(header) - 2) / 2
 
             for row in csvreader:
-                student_id, _ = row[1].split('@')
+                s_id, _ = row[1].split('@')
                 frequency = 100 * row.count('Concluído') // activities
-                self[student_id] = {'Name': row[0],
+                self[s_id] = {'Name': row[0],
                                     'Frequência': frequency,
                                     'Faltas': 100 - frequency}
 
-        super().__init__()
-
 
 class QuizResponse(MoodleBase):
-    """Handles quiz responses.
+    """Processa o relatório de respostas de um questionário.
 
-    Processes a Moodle report data to extract quiz responses information.
-    Extends dict, so keys are the student IDs and each item has the following
-    structure:
-        {'Name': (str - student full name),
-         'Quiz': (dict)
-            {q: (int - question number in quiz)
-                {'attempt': (str - attempt value),
-                 'answer': (str - correct answer value)}}}
+    Acrescenta "Quiz", um dicionário cujas chaves são índices das questões no
+    questionário e os valores outro dicionário com duas chaves: 'attempt' que
+    define a resposta do discente e 'answer' que define a resposta correta.
 
-    To get the data file:
-       1. Access the quiz response report via:
-           Quiz > Cog (Settings) > Results > Responses.
-       2. Select the desired respondent group.
-       3. Select only "attempts that are finished".
-       4. Select only the "response" and the "right answer".
-       5. Show the report.
-       6. Download the JSON file (UTF-8 .json).
+    Para obter o arquivo:
+        1. Acesse o relatório via:
+            Questionário > Configurações (engrenagem) > Respostas
+        2. Selecione o grupo desejado, se for o caso.
+        3. Selecione as 'Tentativas que estão' 'Finalizada'.
+        4. Mostre apenas 'resposta' e 'resposta correta'.
+        5. Mostre o relatório.
+        6. Faça download do arquivo no formato JSON (UTF-8 .json).
     """
 
-    def __init__(self, file, ignore_list=[]):
-        """Constructor.
+    def __init__(self, file):
+        """Construtor.
 
-        Loads the data from the file into instance.
+        Carrega os dados do arquivo.
 
-        Arguments:
-        file -- the JSON file to read from.
+        Argumentos:
+        file -- o arquivo JSON a ser lido.
         """
 
-        import json
-
-        self.data = {}
         with open(file) as f:
             data = json.load(f)
 
         for d in data[0]:
-            student_id, _ = d[2].split('@')
-            self[student_id] = {'Name': f'{d[1]} {d[0]}',
-                                'Quiz': {q: {'attempt': d[(2 * q) + 8].strip(' \r\n'),
-                                             'answer': d[(2 * q) + 9].strip(' \r\n')}
-                                         for q in range(len(d[8::2]))
-                                         if q not in ignore_list}}
+            s_id, _ = d[2].split('@')
+            self[s_id] = {'Name': f'{d[1]} {d[0]}',
+                          'Quiz': {q: {'attempt': d[i].strip(' \r\n'),
+                                       'answer': d[i + 1].strip(' \r\n')}
+                                   for q, i in enumerate(range(8, len(d), 2))}}
+                          # 'Quiz': {q: {'attempt': d[(2 * q) + 8].strip(' \r\n'),
+                          #              'answer': d[(2 * q) + 9].strip(' \r\n')}
+                          #          for q, i in range(len(d[8::2]))}}
 
-        super().__init__()
-
-    def _make_header(self, student_info, file_type):
-        if file_type == 'py':
+    def _make_header(self, student_info, ext):
+        if ext == 'py':
             return '\n'.join(f'# {i}' for i in student_info)
 
-        if file_type in ['c', 'cpp']:
+        if ext in ['c', 'cpp']:
             cmt = '\n   '.join(student_info)
             return f'/* {cmt} */'
 
         return ' '.join(student_info)
 
     def _has_response(self, content):
-        return content[-1] != '-' or content.count('\n') > 2
+        return content.count('\n') > 2
 
-    def to_files(self, path, file_type):
-        for student_id, info in self.items():
-            student_info = [info["Name"], f'{student_id}']
-            header = f'{self._make_header(student_info, file_type)}\n\n'
+    def to_files(self, path, ext='py', ignore_list=[]):
+        """Grava cada resposta em um arquivo específico.
+
+        Argumentos:
+        path -- diretório para armazenar os arquivos.
+        ext -- extensão do arquivo a conter a resposta.
+        ignore_list -- lista com índices de questões que devem ser ignoradas.
+        """
+
+        for s_id, info in self.items():
+            student_info = [info["Name"], f'{s_id}']
+            header = f'{self._make_header(student_info, ext)}\n\n'
 
             for q, src in info['Quiz'].items():
-                if self._has_response(src.get('attempt', '-')):
+                if q not in ignore_list and self._has_response(src.get('attempt', '')):
                     q_dir = os.path.join(path, f'Q{q}')
                     os.makedirs(q_dir, exist_ok=True)
                     response_file = os.path.join(q_dir,
-                                                 f'{student_id}.{file_type}')
+                                                 f'{s_id}.{ext}')
                     with open(response_file, 'w') as f:
                         f.write(f'{header}\n\n{src["attempt"]}')
 
-                    response_file = os.path.join(q_dir, f'CORRECT.{file_type}')
+                    response_file = os.path.join(q_dir, f'CORRECT.{ext}')
                     if not os.path.isfile(response_file):
                         with open(response_file, 'w') as f:
                             f.write(src['answer'])
 
 
 locale.setlocale(locale.LC_ALL, '')
-
-
-# if __name__ == '__main__':
-#     import sys
-
-#     # Check grades.
-#     grades = Grades(sys.argv[1])
-#     print(grades)
-
-#     # Check progress.
-#     progress = Progress(sys.argv[2])
-#     print(progress)
-#     print(progress['180110730'])
-
-#     # Check responses & write to files.
-#     responses = QuizResponse(sys.argv[3])
-#     print(responses)
-#     print(responses['180110730'])
-#     responses.to_files('ResponseDir', 'py')
-
-#     # Cross reference.
-#     grades = Grades(sys.argv[1])
-#     progress = Progress(sys.argv[2])
-#     responses = QuizResponse(sys.argv[3], [0])  # ignore first
-#     for student_id in grades:
-#         print(grades[student_id]['Name'])
-#         if student_id in progress:
-#             print(progress[student_id]['Name'])
-#         if student_id in responses:
-#             print(responses[student_id]['Name'])
