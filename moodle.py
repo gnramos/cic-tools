ff500078b26d72e4a795a933993a747c684b7e15
gnramos <ramos@gnramos.com>
2021-05-14 08:56:07 -0300

    Refatoração da classe base para arquivo próprio.

--- moodle.py
+++ moodle.py
@@ -1,30 +1,11 @@
 import csv
 import json
-import locale
 import os
 import re
+import utils
 
 
-class MoodleBase(dict):
-    """Classe base para manipulação de dados de relatórios do Moodle.
-
-    Funciona como um dicionário, de modo que as chaves são as matrículas e cada
-    item é um dicionário com pelo menos a chave 'Name' contendo o nome do
-    discente.
-
-    """
-
-    def __str__(self):
-        return '\n'.join(f'{self[k]["Name"]}, {k}' for k in self.sorted_keys())
-
-    def sorted_keys(self):
-        """Itera pelas chaves do dicionário em ordem alfabética."""
-        for k in sorted(self.keys(),
-                        key=lambda x: locale.strxfrm(self[x]['Name'])):
-            yield k
-
-
-class Grades(MoodleBase):
+class Grades(utils.Students):
     """Processa o relatório de notas discentes no curso.
 
     Acrescenta as notas grades_idx das atividades avaliadas e afinal do discente.
@@ -62,7 +43,7 @@ class Grades(MoodleBase):
                     self[s_id][header[i]] = float(row[i])
 
 
-class Participants(MoodleBase):
+class Participants(utils.Students):
     """Processa o relatório de participação discente no curso.
 
     Acrescenta a chave 'Group' que indica os grupos (turma) do discente.
@@ -95,7 +76,7 @@ class Participants(MoodleBase):
             self[s_id] = {'Name': name, 'Group': group}
 
 
-class Progress(MoodleBase):
+class Progress(utils.Students):
     """Processa o relatório de progresso de discentes no curso.
 
     Acrescenta "Frequência" e "Faltas" do discente, em termos percentuais.
@@ -130,7 +111,7 @@ class Progress(MoodleBase):
                               'Faltas': 100 - frequency}
 
 
-class QuizResponse(MoodleBase):
+class QuizResponse(utils.Students):
     """Processa o relatório de respostas de um questionário.
 
     Acrescenta "Quiz", um dicionário cujas chaves são índices das questões no
@@ -179,13 +160,13 @@ class QuizResponse(MoodleBase):
     def _has_response(self, content):
         return content.count('\n') > 1
 
-    def to_files(self, path, ext='py', ignore_list=[]):
+    def to_files(self, path, ext='py', ignore=[]):
         """Grava cada resposta em um arquivo específico.
 
         Argumentos:
         path -- diretório para armazenar os arquivos.
         ext -- extensão do arquivo a conter a resposta.
-        ignore_list -- lista com índices de questões que devem ser ignoradas.
+        ignore -- lista com índices de questões que devem ser ignoradas.
         """
 
         for s_id, info in self.items():
@@ -193,7 +174,7 @@ class QuizResponse(MoodleBase):
             header = f'{self._make_header(student_info, ext)}\n\n'
 
             for q, src in info['Quiz'].items():
-                if (q not in ignore_list and
+                if (q not in ignore and
                         self._has_response(src.get('attempt', ''))):
                     q_dir = os.path.join(path, f'Q{q}')
                     os.makedirs(q_dir, exist_ok=True)
@@ -206,6 +187,3 @@ class QuizResponse(MoodleBase):
                     if not os.path.isfile(response_file):
                         with open(response_file, 'w') as f:
                             f.write(src['answer'])
-
-
-locale.setlocale(locale.LC_ALL, '')
