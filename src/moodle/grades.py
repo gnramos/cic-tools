0302a0d659881b76e5f0f34c504d4cfe5b50b04c
gnramos <ramos@gnramos.com>
2021-11-03 15:51:58 -0300

    Ajuste na descrição de como obter o relatório para inclusão de usuários suspensos.

--- src/moodle/grades.py
+++ src/moodle/grades.py
@@ -8,9 +8,11 @@ Para obter o arquivo:
     2. Selecione o formato "Arquivo texto" (aba).
     3. Selecione o grupo desejado, se for o caso.
     4. Selecione os itens que se deseja exportar.
-    5. Selecione o formato de exportação "Real" com separador "Vírgula".
+    5. Não selecione o formato de exportação "Excluir usuários suspensos"."
+    6. Selecione o formato de exportação "Real" com separador "Vírgula".
 """
 
+from argparse import ArgumentParser
 import csv
 import unicodedata
 
@@ -23,6 +25,7 @@ def read(file, total_only=False):
     total_only -- booleano indicando se considera apenas as notas consolidadas
                   (total).
     """
+    EMAIL_IDX, FIRST_GRADE_IDX = 5, 7
     grades = {}
     with open(file) as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -36,30 +39,30 @@ def read(file, total_only=False):
                           if col.endswith(' total (Real)')]
             grades_idx.append(header.index('Nota Final (Real)'))
         else:
-            grades_idx = range(6, len(header))
+            grades_idx = range(FIRST_GRADE_IDX, len(header))
 
         header = [h.replace(' (Real)', '') for h in header]
 
         for row in csvreader:
-            if (s_id := row[5].split('@')[0]).isnumeric(): # e-mail
+            if (s_id := row[EMAIL_IDX].split('@')[0]).isnumeric():
                 grades[s_id] = {'Name': f'{row[0]} {row[1]}', 'Grades': {}}
                 for i in grades_idx:
-                    grade = 0 if row[i] == '-' else float(row[i].replace(',',
-                                                                         '.'))
+                    grade = '0' if row[i] == '-' else row[i]
                     grades[s_id]['Grades'][header[i]] = grade
 
     return grades
 
 
-def main():
-    """Processa argumentos da linha de comando."""
-
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser(read.__doc__.split('\n')[0])
-    parser.add_argument('file', help='O arquivo CSV a ser lido.')
+def parser_add_argument(parser):
     parser.add_argument('-t', '--total_only', action='store_true',
                         help='Considerar apenas notas consolidadas (total).')
+
+
+def main():
+    """Processa argumentos da linha de comando."""
+    parser = ArgumentParser(read.__doc__.split('\n')[0])
+    parser.add_argument('file', help='O arquivo CSV a ser lido.')
+    parser_add_argument(parser)
 
     args = parser.parse_args()
     grades = read(args.file, args.total_only)
